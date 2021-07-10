7b89b94fe86fc65dc13910da537e6d24bf2b40db
habatoo <habatoo@gmail.com>
2021-07-10 13:42:50 +0700

    add user permission method

--- src/main/java/dwarf/framework/security/ApplicationSecurityConfig.java
+++ src/main/java/dwarf/framework/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package dwarf.framework.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,9 +29,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll() // white list url
                 .antMatchers("/api/*").hasAnyRole(USER.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(ApplicationUserPermission.USER_DELETE.getPermission()) // urls for permission
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(ApplicationUserPermission.USER_DELETE.getPermission()) // urls for permission
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.USER_DELETE.getPermission()) // urls for permission
+                .antMatchers(HttpMethod.PATCH,"/management/api/**").hasAuthority(ApplicationUserPermission.USER_DELETE.getPermission()) // urls for permission
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), EVENT_ADMIN.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -43,19 +50,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails admin = User.builder()
                 .username("Admin")
                 .password(passwordEncoder.encode("password"))
-                .roles(ADMIN.name()) // ROLE_ADMIN
+                //.roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails eventAdmin = User.builder()
                 .username("EventAdmin")
                 .password(passwordEncoder.encode("password"))
-                .roles(EVENT_ADMIN.name()) // ROLE_EVENT_ADMIN
+                //.roles(EVENT_ADMIN.name()) // ROLE_EVENT_ADMIN
+                .authorities(EVENT_ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails user = User.builder()
                 .username("User")
                 .password(passwordEncoder.encode("password"))
-                .roles(USER.name()) // ROLE_USER
+                //.roles(USER.name()) // ROLE_USER
+                .authorities(USER.getGrantedAuthorities())
                 .build();
         return new InMemoryUserDetailsManager(admin, eventAdmin, user);
     }
--- src/main/java/dwarf/framework/security/ApplicationUserRole.java
+++ src/main/java/dwarf/framework/security/ApplicationUserRole.java
@@ -1,7 +1,13 @@
 package dwarf.framework.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import static dwarf.framework.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
@@ -31,6 +37,14 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
